70bc373ecec630fa4e65704987e9e0ec5134b0e1
wesantos <wesantosdev@gmail.com>
2025-08-14 17:07:48 -0300

    update

---
+++ 07-08-2025/ex_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name1[] = "Waliston";
+    char name2[] = "Euripedes";
+    char name3[] = "dos Santos";
+
+    printf("%s \n %s \n %s", name1, name2,name3);
+
+    return 0;
+}
