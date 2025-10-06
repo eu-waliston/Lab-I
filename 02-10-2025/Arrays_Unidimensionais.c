f2840ef3f3e91b87808a79f23d9ff09aaabbe592
Waliston <wesantosdev@gmail.com>
2025-10-06 19:46:48 -0300

    update

--- 02-10-2025/Arrays_Unidimensionais.c
+++ 02-10-2025/Arrays_Unidimensionais.c
@@ -1,15 +1,74 @@
 #include <stdio.h>
 
 #define TAMANHO 10
+#define TAM 50
+#define MAX 30
 
-int main() {
+// Inicialização por atribuição
+void inicializacao_po_atributos()
+{
     int i, numeros[TAMANHO];
 
-    for (i = 0; i < TAMANHO; i ++) {
+    for (i = 0; i < TAMANHO; i++)
+    {
         numeros[i] = i * 2;
     }
 
-    for (i = 0; i < TAMANHO; i++) {
+    for (i = 0; i < TAMANHO; i++)
+    {
         printf("%d", numeros[i]);
     }
+}
+
+// Leitura com scanf
+void leitura_com_scanf()
+{
+    int i, vet[TAM];
+
+    printf("diite %d valores: \n", TAM);
+    for (i = 0; i < TAM; i++)
+    {
+        scanf("%d", &vet[i]);
+    }
+
+    for (i = 0; i < TAM; i++)
+    {
+        printf("Valor %d: %d\n", i + 1, vet[i]);
+    }
+}
+
+// Faça um programa que leia 30 valores, calcule a sua média
+// aritmética e imprima a média e os valores iguais ou superiores à
+// média
+void leitura_de_valores()
+{
+    int i, soma = 0, vetor[MAX];
+    double media;
+
+    printf("Digite %d valores (inteiros): \n", MAX);
+    for (i = 0; i < MAX; i++)
+    {
+        scanf("%d", &vetor[i]);
+        soma = soma + vetor[i];
+    }
+
+    media = (double)soma / MAX;
+    printf("\nMEdia = %8.2lf\n", media);
+    printf("\n\nValores iguais ou superiores a media:\n");
+    for (i = 0; i < MAX; i++)
+    {
+        if (vetor[i] >= media)
+        {
+            printf("%4d\n", vetor[i]);
+        }
+    }
+}
+
+int main()
+{
+    inicializacao_po_atributos();
+    leitura_com_scanf();
+    leitura_de_valores();
+
+    return 0;
 }
---
+++ 02-10-2025/Exercicios.md
@@ -0,0 +1,21 @@
+# Exercicios
+
+1) Leia valores para um vetor de 10 elementos. A seguir, leia um valor e
+imprima o número de ocorrências desse valor no vetor.
+
+2) Leia um vetor de reais (positivos ou nulos) e o escreva. A seguir,
+substitua os valores iguais a zero por -99 e escreva novamente o vetor.
+
+3) Leia um vetor de inteiros e o escreva. A seguir, apresente o menor
+valor, juntamente com sua posição no vetor.
+
+4) Leia um conjunto de 50 valores, coloque-os em 2 vetores
+classificando-os como pares ou ímpares. Imprimir os 2 vetores.
+
+5) Leia um vetor de 10 posições e mostre-o. A seguir, troque o primeiro
+elemento com o último, o segundo com o penúltimo, o terceiro com o
+antepenúltimo, e assim sucessivamente. Mostre o vetor após a troca.
+
+6) Leia 2 vetores A e B de tamanho 10 e os escreva. Crie, a seguir, um
+vetor C de 20 posições, que contenha os elementos dos vetores A e B
+em ordem decrescente.
